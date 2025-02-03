987caf560268cd7ce528e04352b93799d495ddab
soala144 <soala144@gmail.com>
2025-02-02 18:12:29 -0800

    logic done

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-let expenses = []
+let expense = []
 
 // getting values from html 
 const categoryInput = document.getElementById("category")
 const amountInput = document.getElementById("amount")
 const addExpenseButton = document.getElementById("addExpense")
-
+const expenseGroup = {}
 addExpenseButton.addEventListener("click", function(){
     const category = categoryInput.value
-    const amount = parseInt(amountInput.value) 
+    const amount = parseFloat(amountInput.value) 
 
     if(isNaN(amount) || amount <=0){
         alert("Please enter a solid amount")
         return
     }
 
-    const expense = {category, amount}
+    const expense = {amount}
+    
+    if(!expenseGroup[category]){
+        expenseGroup[category] = []
+    }
 
-    expenses.push(expense)
-    console.log(expenses)
+    expenseGroup[category].push(expense)
+    updateExpenseList()
+    amountInput.innerHTML =""
+
 })
+const expensList = document.getElementById("expenseList")
+function updateExpenseList(){
+    expensList.innerHTML = ""
+
+    Object.keys(expenseGroup).forEach(category => {
+        let totalAmount = expenseGroup[category].reduce((sum, expense) => sum + expense.amount, 0)
+        const categoryHeader = document.createElement("h3")
+        categoryHeader.textContent = `${category} (Total: $${totalAmount})`
+        expensList.appendChild(categoryHeader)
+
+
+        expenseGroup[category].forEach((expense, index) =>{
+            const li = document.createElement("li")
+            li.textContent =`$${expense.amount}`
+            
+
+            const deleteBtn = document.createElement("span")
+            deleteBtn.innerHTML = "&times"
+            deleteBtn.style.cursor ="pointer"
+            deleteBtn.style.color = "red"
+            deleteBtn.style.fontSize = "18px"
+            deleteBtn.style.marginLeft = "10px"
+            deleteBtn.addEventListener("click", () => deleteExpense(category, index))
+
+            li.appendChild(deleteBtn)
+            expensList.appendChild(li)
+        })
+    })
+}
+
+function deleteExpense(category,  index){
+    expenseGroup[category].splice(index, 1)
+
+    if(expenseGroup[category].length === 0){
+        delete expenseGroup[category]    
+    }
+}
